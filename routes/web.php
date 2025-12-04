bd83e6648a4fd95221ebfcd048d433aab4107948
Dante Ramirez Lopez <danterldk@gmail..com>
2025-12-04 09:39:29 -0500

    links activos

--- routes/web.php
+++ routes/web.php
@@ -83,9 +83,9 @@ Route::middleware(['setData'])->group(function () {
     Auth::routes();
 
     // Route::get('/business/register', [BusinessController::class, 'getRegister'])->name('business.getRegister');
-    // Route::post('/business/register', [BusinessController::class, 'postRegister'])->name('business.postRegister');
-    // Route::post('/business/register/check-username', [BusinessController::class, 'postCheckUsername'])->name('business.postCheckUsername');
-    // Route::post('/business/register/check-email', [BusinessController::class, 'postCheckEmail'])->name('business.postCheckEmail');
+    Route::post('/business/register', [BusinessController::class, 'postRegister'])->name('business.postRegister');
+    Route::post('/business/register/check-username', [BusinessController::class, 'postCheckUsername'])->name('business.postCheckUsername');
+    Route::post('/business/register/check-email', [BusinessController::class, 'postCheckEmail'])->name('business.postCheckEmail');
 
     Route::get('/invoice/{token}', [SellPosController::class, 'showInvoice'])
         ->name('show_invoice');
